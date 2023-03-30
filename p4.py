8372e494e3bd2c908612ea39e8edce0cc06a82b1
Bradly M <brad1864@gmail.com>
2023-03-30 08:17:13 -0400

    beginning 4th video

---
+++ p4.py
@@ -0,0 +1,14 @@
+# video 4 -> Batches, Layers and Objects
+import numpy as np
+
+inputs = [1.0, 2.0, 3.0, 2.5] # vector
+
+weights = [[0.2, 0.8, -0.5, 1.0], # matrix containing vectors
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+bias = [2, 3, 0.5]
+
+output = np.dot(weights, inputs) + bias
+print(output)
+
