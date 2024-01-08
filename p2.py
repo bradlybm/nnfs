28c86d95006ea1ead6e95d71eb6012e2fff1f6bb
bradlybm <113479233+bradlybm@users.noreply.github.com>
2024-01-07 23:24:46 -0500

    code spacing

--- p2.py
+++ p2.py
@@ -16,9 +16,9 @@ bias2 = 3
 bias3 = 0.5
 
 # beacuse I am modeling the output layer (3 different neurons) the output will look like the input (except only with 3 values)
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
 print(output)
 
 # In order to change the output value I can the weights and biases (which is the struggle of NN and ML...intersting)
