03ed131cdeb171f74b3fd5615695b5f116748472
bradlybm <113479233+bradlybm@users.noreply.github.com>
2024-01-21 14:10:38 -0500

    finishing touches

--- p4_1.py
+++ p4_1.py
@@ -17,8 +17,15 @@ class Layer_Dense: # 21:06
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4, 5) # the output of this (5)
+layer2 = Layer_Dense(5, 2) # has to be the input of this however the output of layer 2 can be any number i want.
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
 
 
-print(0.10 * np.random.randn(4,3))
